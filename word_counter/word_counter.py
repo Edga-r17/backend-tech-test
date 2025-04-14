c5a719f3982007df2d290dec55e06ab49a51f954
Edgar-Rojas <edgar@balam.rocks>
2025-04-14 11:31:19 -0600

    busca ocurrencias letra por letra y valida delimitadores

--- word_counter/word_counter.py
+++ word_counter/word_counter.py
@@ -18,8 +18,40 @@ def convertir_a_minusculas(texto):
             resultado += caracter
     return resultado
 
+def es_letra(caracter):
+    return ('a' <= caracter <= 'z') or ('á' <= caracter <= 'ú') or ('ñ' == caracter)
+
+def contar_ocurrencias(parrafo, palabra):
+    ocurrencias = 0
+    i = 0
+
+    while i <= len(parrafo) - len(palabra):
+        match = True
+
+        for j in range(len(palabra)):
+            if parrafo[i + j] != palabra[j]:
+                match = False
+                break
+
+        if match:
+            antes = parrafo[i - 1] if i > 0 else ' '
+            despues = parrafo[i + len(palabra)] if i + len(palabra) < len(parrafo) else ' '
+
+            if not es_letra(antes) and not es_letra(despues):
+                ocurrencias += 1
+                i += len(palabra)  
+            else:
+                i += 1
+        else:
+            i += 1
+
+    return ocurrencias
+
 parrafo = convertir_a_minusculas(parrafo)
 palabra_buscada = convertir_a_minusculas(palabra_buscada)
+resultado = contar_ocurrencias(parrafo, palabra_buscada)
+
 
 print(parrafo)
 print(palabra_buscada)
+print(f"'{palabra_buscada}' aparece {resultado} veces.")
